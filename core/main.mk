df6a31c76ec0b73b64758fdf7c5ea192dfb03c48
Chet Kener <Cl3Kener@gmail.com>
2015-01-23 11:22:32 -0500

    Mock Location should be off!
    * Unless you don't want people to know your exact location.  Users can try it then if they like
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -384,7 +384,7 @@ else # !user_variant
   # Set device insecure for non-user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
   # Allow mock locations by default for non user builds
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=0
 endif # !user_variant
 
 ifeq (true,$(strip $(enable_target_debugging)))
