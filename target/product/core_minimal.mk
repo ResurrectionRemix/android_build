7159e6efc5052ab21b7a3194961e8c664b759b0f
arter97 <qkrwngud825@gmail.com>
2015-05-16 19:50:01 +0900

    Add PRODUCT_PACKAGES for Google's prebuilt WebView
    https://github.com/arter97/android_frameworks_webview/blob/cm-12.1/README.mkdn
    Signed-off-by: arter97 <qkrwngud825@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -74,6 +74,8 @@ PRODUCT_PACKAGES += \
     uncrypt \
     voip-common \
     webview \
+        libwebviewchromium_plat_support \
+        libwebviewchromium_loader \
     wifi-service
 
 PRODUCT_COPY_FILES += \
