5dcca1e7556fcb921f023933211756057d799ae4
TheBr0ken <jtoro2716@gmail.com>
2015-06-20 11:49:11 -0400

    Clean option: deviceclean
    This removes a specific device from /out/target/product dir. For this to work properly you MUST lunch
    the device first or else clean wont know what device you want to remove
    
    Change-Id: I15ccd7d3c37f9cde1614871a78e4074d14cb3ded

--- core/main.mk
+++ core/main.mk
@@ -74,7 +74,7 @@ FORCE:
 
 # These goals don't need to collect and include Android.mks/CleanSpec.mks
 # in the source tree.
-dont_bother_goals := clean clobber dataclean installclean \
+dont_bother_goals := clean clobber dataclean deviceclean installclean \
     help out \
     snod systemimage-nodeps \
     stnod systemtarball-nodeps \
@@ -1195,6 +1195,12 @@ recoveryclean:
 rootclean:
 	@rm -rf $(OUT_DIR)/target/product/*/root/
 	@echo -e ${CL_GRN}"All root components erased"${CL_RST}
+
+# Clears out only target files
+.PHONY: deviceclean
+deviceclean:
+	@rm -rf $(OUT_DIR)/target/product/$(TARGET_DEVICE)
+	@echo -e ${CL_GRN}"$(TARGET_DEVICE) files removed successfully"${CL_RST}
 
 # The rules for dataclean and installclean are defined in cleanbuild.mk.
 
