2097d0ce5d7f7e39eb877cf4da57a599072ac983
varund7726 <date.varun123@gmail.com>
2015-08-19 15:38:15 +0530

    Revert "JustArchi's ArchiDroid Optimizations V4.1"
    This reverts commit 3f5b5517f1c5ba710dea0eee39448d8522c3571b.

--- core/combo/TARGET_linux-arm64.mk
+++ core/combo/TARGET_linux-arm64.mk
@@ -119,17 +119,10 @@ TARGET_GLOBAL_LDFLAGS += $(BOARD_GLOBAL_LDFLAGS)
 
 TARGET_GLOBAL_CPPFLAGS += -fvisibility-inlines-hidden
 
-# ArchiDroid
-include $(BUILD_SYSTEM)/archidroid.mk
-
-$(combo_2nd_arch_prefix)TARGET_GLOBAL_CFLAGS += $(ARCHIDROID_GCC_CFLAGS)
-$(combo_2nd_arch_prefix)TARGET_GLOBAL_CPPFLAGS += $(ARCHIDROID_GCC_CPPFLAGS)
-$(combo_2nd_arch_prefix)TARGET_GLOBAL_LDFLAGS += $(ARCHIDROID_GCC_LDFLAGS)
-
 # More flags/options can be added here
 TARGET_RELEASE_CFLAGS := \
 			-DNDEBUG \
-			$(ARCHIDROID_GCC_CFLAGS_ARM) \
+			-O2 -g \
 			-Wstrict-aliasing=2 \
 			-fgcse-after-reload \
 			-frerun-cse-after-loop \
