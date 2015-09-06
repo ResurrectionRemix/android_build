3e768bbb3a3b40e3988ec2646798088387f26438
Akhil Narang <akhilnarang.1999@gmail.com>
2015-09-06 14:54:00 +0530

    build: remove remains of prebuilt chromium
    Change-Id: Iff789426326a9345956068bf37dfe4d5f2ba665b
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -649,13 +649,6 @@ function lunch()
 
     echo
 
-    if [[ $USE_PREBUILT_CHROMIUM -eq 1 ]]; then
-        chromium_prebuilt
-    else
-        # Unset flag in case user opts out later on
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=""
-    fi
-
     fixup_common_out_dir
 
     set_stuff_for_environment
@@ -2600,20 +2593,6 @@ function pez {
     return $retval
 }
 
-function chromium_prebuilt() {
-    T=$(gettop)
-    export TARGET_DEVICE=$(get_build_var TARGET_DEVICE)
-    hash=$T/prebuilts/chromium/$TARGET_DEVICE/hash.txt
-
-    if [ -r $hash ] && [ $(git --git-dir=$T/external/chromium_org/.git --work-tree=$T/external/chromium_org rev-parse --verify HEAD) == $(cat $hash) ]; then
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=yes
-        echo "** Prebuilt Chromium is up-to-date; Will be used for build **"
-    else
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=no
-        echo "** Prebuilt Chromium out-of-date/not found; Will build from source **"
-    fi
-}
-
 function get_make_command()
 {
   echo command make
@@ -2645,27 +2624,6 @@ function mk_timer()
     echo -e " ####"
     echo
     return $ret
-}
-
-function chromium_prebuilt() {
-    T=$(gettop)
-    export TARGET_DEVICE=$(get_build_var TARGET_DEVICE)
-    hash=$T/prebuilts/chromium/$TARGET_DEVICE/hash.txt
-
-    # Colors
-    txtbld=$(tput bold)
-    bldblu=${txtbld}$(tput setaf 4)
-    bldgrn=${txtbld}$(tput setaf 2)
-
-    if [ -r $hash ] && [ $(git --git-dir=$T/external/chromium_org/.git --work-tree=$T/external/chromium_org rev-parse --verify HEAD) == $(cat $hash) ] && [ -f $libsCheck ] && [ -d $appCheck ]; then
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=yes
-        echo -e ${bldblu}"Prebuilt Chromium is up-to-date: ${bldgrn}Will be used for build"${txtrst}
-        echo -e ""
-    else
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=no
-        echo -e ${bldblu}"Prebuilt Chromium out-of-date or not found: ${bldgrn}Will build from source"${txtrst}
-        echo -e ""
-    fi
 }
 
 function make()
