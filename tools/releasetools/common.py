71b6f4ef9497b5a0d7d7c7e098de4e3df4a54c7e
Aren Clegg <aclegg2011@gmail.com>
2015-12-28 22:30:09 -0800

    remove code to fix multirom install issues

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1255,7 +1255,7 @@ class BlockDifference(object):
     if progress:
       script.ShowProgress(progress, 0)
     self._WriteUpdate(script, output_zip)
-    self._WritePostInstallVerifyScript(script)
+   
 
   def WriteVerifyScript(self, script):
     partition = self.partition
