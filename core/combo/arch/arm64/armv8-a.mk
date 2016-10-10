a25512fb5c3f6fb29177c1998c25f15019f18981
Alex Naidis <alex.naidis@linux.com>
2016-10-10 22:55:59 +0200

    build: condition cortex-a53-835769 workaround for the linker
    * This is complimentary to 18e9c5e
    * Without this, the linker would still
      try to apply the workaround for targets
      for which we don't want this to happen.
    * Patch 18e9c5e being incomplete was noticed
      by @arter97
    
    Change-Id: Ifdf3142f3ba3ad80f46e6bad42b8443bfd353f6d
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -7,9 +7,11 @@ endif
 ifneq (,$(filter cortex-a53 default,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags  += -mfix-cortex-a53-835769
 	arch_variant_ldflags := -Wl,--fix-cortex-a53-843419
+	arch_variant_ldflags += -Wl,--fix-cortex-a53-835769
 else
 	arch_variant_cflags  += -mno-fix-cortex-a53-835769
 	arch_variant_ldflags := -Wl,--no-fix-cortex-a53-843419
+	arch_variant_ldflags += -Wl,--no-fix-cortex-a53-835769
 	RS_DISABLE_A53_WORKAROUND := true
 endif
 
