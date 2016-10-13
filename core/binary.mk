4587edcfcfcceee6dfbb176a2feeb4b5066b14e8
Alex Naidis <alex.naidis@linux.com>
2016-10-13 19:02:15 +0200

    build: Do proper optimization during SDCLANG_LTO
    * When optimization is done at linking time,
      the optimization related flags which
      were passed to the compiler aren't respected.
     -> Introduce LOCAL_SDCLANG_LTO_LDFLAGS which
        represent the optimization related flags
        which get passed to the linker during LTO
        per module.
    * If LOCAL_SDCLANG_LTO_LDFLAGS isn't set,
      we assume that the module relies on the
      optimization cflags provided by our platform
      as "default". In this case, we just inherit
      SDCLANG_COMMON_FLAGS from platform to LDFLAGS.
    
    Change-Id: I95ecb4b9d2741a14ae39a9c674d2529c6689ada4
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/binary.mk
+++ core/binary.mk
@@ -503,8 +503,13 @@ my_target_global_ldflags := $($(LOCAL_2ND_ARCH_VAR_PREFIX)CLANG_TARGET_GLOBAL_LD
 
         ifeq ($(LOCAL_SDCLANG_LTO), true)
         ifneq ($(LOCAL_MODULE_CLASS), STATIC_LIBRARIES)
+
+            ifeq ($(strip $(LOCAL_SDCLANG_LTO_LDFLAGS)),)
+                LOCAL_SDCLANG_LTO_LDFLAGS := $(SDCLANG_COMMON_FLAGS)
+            endif
+
             SDCLANG_PRECONFIGURED_FLAGS += -fuse-ld=qcld -flto
-            my_target_global_ldflags += -fuse-ld=qcld -flto
+            my_target_global_ldflags += -fuse-ld=qcld -flto $(LOCAL_SDCLANG_LTO_LDFLAGS)
         endif
         endif
         my_target_global_cflags += $(SDCLANG_COMMON_FLAGS) $(SDCLANG_PRECONFIGURED_FLAGS)
