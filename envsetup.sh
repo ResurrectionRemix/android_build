bc0a136c64d20ed1daf77bb553a304eb35aad63f
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-12-21 08:33:26 +0800

    Build: Cyanogen >> Lineage * Change necessary bits to support our project move
    Change-Id: Ie6a04eec45b6eb9f6f76f29e3163abd3f95f5da6

--- envsetup.sh
+++ envsetup.sh
@@ -1719,6 +1719,6 @@ check_bash_version && {
 }
 
 export ANDROID_BUILD_TOP=$(gettop)
-. vendor/cm/tools/rr_variant.sh && ./vendor/cm/tools/changelog.sh &&
-. vendor/cm/build/envsetup.sh
 
+. $ANDROID_BUILD_TOP/vendor/cm/tools/rr_variant.sh && ./vendor/cm/tools/changelog.sh &&
+. vendor/cm/build/envsetup.sh
