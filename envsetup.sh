87c368ba8efddc26995a3a3589af00cc2385c993
Akhil Narang <akhilnarang.1999@gmail.com>
2018-02-03 08:12:56 +0530

    envsetup: don't call rr.sh
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1749,5 +1749,5 @@ addcompletions
 
 export ANDROID_BUILD_TOP=$(gettop)
 
-. vendor/rr/tools/rr_variant.sh && . vendor/rr/tools/rr_root.sh && ./vendor/rr/tools/changelog.sh && . vendor/rr/build/envsetup.sh && . vendor/rr/tools/rr.sh
+. vendor/rr/tools/rr_variant.sh && . vendor/rr/tools/rr_root.sh && ./vendor/rr/tools/changelog.sh && . vendor/rr/build/envsetup.sh
 
