AOSP-10/build

product: Exclude AOSP apn list on custom build
Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898

target/product/full_base_telephony.mk
@@ -22,8 +22,10 @@
 PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true
 
-PRODUCT_COPY_FILES := \
+ifeq ($(CUSTOM_BUILD),)
+PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+endif
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)

target/product/gsi_common.mk
@@ -74,8 +74,10 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(CUSTOM_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
 
 # NFC:
 #   Provide a libnfc-nci.conf to GSI product

target/product/mainline.mk
@@ -26,4 +26,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
 
+ifeq ($(CUSTOM_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif

ResurrectionRemix/android_build

Signed-off-by: mracar07 <umutcan@umutcanacar.me>

target/product/full_base_telephony.mk
@@ -22,8 +22,10 @@
 PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true
 
-PRODUCT_COPY_FILES := \
+ifeq ($(RR_BUILD),)
+PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+endif
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)

target/product/gsi_common.mk
@@ -77,8 +77,10 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(RR_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
 
 # NFC:
 #   Provide a libnfc-nci.conf to GSI product

target/product/mainline.mk
@@ -30,4 +30,6 @@ PRODUCT_PACKAGES += \
     PhotoTable \
     WallpaperPicker \
 
+ifeq ($(RR_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif